83d73e71419a94f9f5021617a9d01dc9dae3749f
Jason Jenkins <jas1217@gmail.com>
2011-01-26 22:59:34 -0600

    added largest.c

--- largest.c
+++ largest.c
@@ -4,16 +4,40 @@
 
 int main(void)
 {
-	int i1, i2, i3, i4, lg, sm;
-	
+	int i1, i2, i3, i4, lg, sm, lg1, lg2, sm1, sm2;
+
 	printf("Enter four integers: ");
 	scanf("%d %d %d %d", &i1, &i2, &i3, &i4);
-	
-	sm = i1;
-	if (i2 < sm)
-		sm = i2;
-	if (i3 < sm)
-		sm = i3;
-	if (i4 < sm)
-		sm = i4;
+
+
+	if (i1 < i2) {
+		sm1 = i1;
+		lg1 = i2;
+	} else {
+		sm1 = i2;
+		lg1 = i1;
+	}
+
+	if (i3 < i4) {
+		sm2 = i3;
+		lg2 = i4;
+	} else {
+		sm2 = i4;
+		lg2 = i3;
+	}
+
+	if (sm1 < sm2)
+		sm = sm1;
+	else
+		sm = sm2;
+
+	if (lg1 < lg2)
+		lg = lg2;
+	else
+		lg = lg1;
+
+	printf("\nLargest is: %d\n", lg);
+	printf("\nSmallest is: %d\n", sm);
+
+	return 0;
 }
